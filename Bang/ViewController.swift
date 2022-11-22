3b647cb422a53aa35ece679c35212c1daca4d4a3
Adrian <luisadrian@gmail.com>
2022-11-22 14:31:21 -0800

    commit initial

--- Bang/ViewController.swift
+++ Bang/ViewController.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import AVKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    var play : AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        play?.delegate = self
+        
     }
+    
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        print("Sacudir")
+        view.backgroundColor = .orange
+        bangAudio()
+    }
+    
+    func bangAudio(){
+        let urlB = URL(string: Bundle.main.path(forResource: "loud_bang", ofType: "mp3")!)!
+        play = try! AVAudioPlayer(contentsOf: urlB)
+        play?.prepareToPlay()
+        play?.play()
+        
+    
+        
+        
+        
+    }
+    
+
+    
+    
 
 
 }
